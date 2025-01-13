69b5865fbdb4411da535ec49578cf12b559588d9
mobin <mobinfallahi0@gmail.com>
2025-01-13 18:51:54 +0330

    change BucketSort

--- Aloghrithm/src/Sort/BucketSort.java
+++ Aloghrithm/src/Sort/BucketSort.java
@@ -1,21 +1,10 @@
 package Sort;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class BucketSort {
-    public static void insertionSort(List<Float> bucket) {
-        for (int i = 1; i < bucket.size(); ++i) {
-            float key = bucket.get(i);
-            int j = i - 1;
-            while (j >= 0 && bucket.get(j) > key) {
-                bucket.set(j + 1, bucket.get(j));
-                j--;
-            }
-            bucket.set(j + 1, key);
-        }
-    }
-
     public static void bucketSort(float[] arr) {
         int n = arr.length;
 
@@ -30,7 +19,7 @@ public class BucketSort {
         }
 
         for (int i = 0; i < n; i++) {
-            insertionSort(buckets[i]);
+            Collections.sort(buckets[i]);
         }
 
         int index = 0;
